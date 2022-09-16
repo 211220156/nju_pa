13563143536e3706cfece1b0d60ea0848717cf1e
tracer-pa-public <tracer@njuics.org>
2022-09-16 20:14:03 +0800

    > clean > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:14:03 up 58 days, 11:40,  1 user,  load average: 2.24, 2.29, 2.33 > unitime > 4984f4f34ad60e3d56ba265fb072ad8d270ec0c2

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -29,8 +29,9 @@ void set_ZF(uint32_t res, size_t data_size)
 
 void set_SF(uint32_t res, size_t data_size)
 {
-    res = sign_ext(res & (0xFFFFFFFF >> (32 - data_size)) ,data_size);
-    cpu.eflags.SF = sign(res);
+//    res = sign_ext(res & (0xFFFFFFFF >> (32 - data_size)) ,data_size);
+//    cpu.eflags.SF = sign(res);
+    cpu.eflags.SF = (res >> (data_size - 1)) & 1;
 }
 
 void set_OF_add(uint32_t res, uint32_t src, uint32_t dest, size_t data_size)
