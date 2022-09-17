58d81ea534b3f9885bb8789fcae29a59bc691faa
tracer-pa-public <tracer@njuics.org>
2022-09-17 18:44:03 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:44:03 up 59 days, 10:10,  1 user,  load average: 2.05, 2.51, 2.49 > unitime 1663411443034 > 16c93273a803116db4739f2f9439d68246cd7804

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -335,12 +335,14 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size)
 #else
 	
 	src = src & (0xFFFFFFFF >> (32 - data_size));
-    uint32_t high = (dest >> data_size) << data_size;
+/*    uint32_t high = (dest >> data_size) << data_size;
     uint32_t low = dest & (0xFFFFFFFF >> (32 - data_size));
     
     cpu.eflags.CF = (low >> (data_size - src)) % 2;
     low = (low << src) & (0xFFFFFFFF >> (32 - data_size));
-    return high | low;
+    return high | low;*/
+    cpu.eflags.CF = (dest >> (data_size - src)) % 2;
+    return dest << src;
 	
 #endif
 }
