1c9e281dca3a7bbc42eb46be7d3638fd463ab56a
tracer-pa-public <tracer@njuics.org>
2022-09-17 12:07:01 +0800

    > clean > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 12:07:01 up 59 days,  3:33,  1 user,  load average: 2.39, 2.40, 2.38 > unitime > 5e14f020ef34cb92787341d447ab7513952701b4

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -148,8 +148,8 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	
 	set_CF_sub(res, dest, data_size);
 	set_PF(res);
-	set_ZF(res);
-	set_SF(res);
+	set_ZF(res, data_size);
+	set_SF(res, data_size);
 	set_OF_sub(res, src, dest, data_size);
 	
 	return res & (0xFFFFFFFF >> (32 - data_size));//高位清零
