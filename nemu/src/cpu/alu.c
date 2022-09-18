1a8ed6f073f6bd83464bab0013d10897a69d6928
tracer-pa-public <tracer@njuics.org>
2022-09-18 11:33:51 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:33:51 up 60 days,  3:00,  1 user,  load average: 2.02, 2.27, 2.27 > unitime 1663472031516 > c83b1c32db65ca7ca9e9c377975cd17574bfd5c8

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -202,11 +202,40 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
 	
 	src = src & (0xFFFFFFFF >> (32 - data_size));
     dest = dest & (0xFFFFFFFF >> (32 - data_size));
-    
     uint64_t res = 0, dest_64 = dest, src_64 = src;
-    
     res = dest_64 * src_64;
-    
+    switch (data_size){
+        case 8: {
+            uint64_t AH = res >> 8;
+            if (AH == 0){
+                cpu.eflags.CF = 0;
+                cpu.eflags.OF = 0;
+            } else {
+                cpu.eflags.CF = 1;
+                cpu.eflags.OF = 1;
+            }
+        }
+        case 16: {
+            uint64_t DX = res >> 16;
+            if (DX == 0){
+                cpu.eflags.CF = 0;
+                cpu.eflags.OF = 0;
+            } else {
+                cpu.eflags.CF = 1;
+                cpu.eflags.OF = 1;
+            }
+        }
+        default:{
+            uint64_t EDX = res >> 32;
+            if (EDX == 0){
+                cpu.eflags.CF = 0;
+                cpu.eflags.OF = 0;
+            } else {
+                cpu.eflags.CF = 1;
+                cpu.eflags.OF = 1;
+            }
+        }
+    }
     return res;
     
 	
