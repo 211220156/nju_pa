f21c121b0dbc7c100f34cb08c12b8eed001c6cab
tracer-pa-public <tracer@njuics.org>
2022-10-12 20:29:00 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:29:00 up 84 days, 11:55,  2 users,  load average: 2.42, 2.51, 2.45 > unitime 1665577740540 > 47d65de7c510662475ecdf25be6b5aab7567e0b7

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -20,12 +20,12 @@ make_instr_func(call_near)
     opr.val = cpu.eip;
     operand_write(&opr);
     
-    if (rel.data_size == 16){
+/*    if (rel.data_size == 16){
         cpu.eip = (cpu.eip + sign_ext(rel.val, data_size)) & 0x0000ffff;
     } else {
         cpu.eip = cpu.eip + rel.val;
-    }
-//    int offset = sign_ext(rel.val, data_size);
-//    cpu.eip += offset;
+    }*/
+    int offset = sign_ext(rel.val, data_size);
+    cpu.eip += offset;
     return 1 + data_size / 8;
 }
--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -2,3 +2,16 @@
 /*
 Put the implementations of `ret' instructions here.
 */
+make_instr_func(ret_near){
+    OPERAND opr;
+    opr.type = OPR_MEM;
+    opr.data_size = data_size;
+    opr.addr = cpu.gpr[4].val;
+    operand_read(&opr);
+    cpu.gpr[4].val += (data_size / 8);
+    
+    cpu.eip = opr.val;
+    if (data_size == 16){
+        cpu.eip &= 0x0000ffff;
+    }
+}
