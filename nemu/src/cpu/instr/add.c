c753a43c9f931b39f99c6ed618668aaf88912d7d
tracer-pa-public <tracer@njuics.org>
2022-10-12 20:50:18 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:50:18 up 84 days, 12:16,  2 users,  load average: 2.18, 2.16, 2.21 > unitime 1665579018246 > 9a68371b959f666cc2d997e7ba75d12dd835e881

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -6,7 +6,7 @@ static void instr_execute_2op()
 {
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    opr_dest.val = alu_add(opr_src.val, opr_dest.val, data.size);
+    opr_dest.val = alu_add(opr_src.val, opr_dest.val, data_size);
     operand_write(&opr_dest);
 }
 make_instr_impl_2op(add, i, rm, v)
