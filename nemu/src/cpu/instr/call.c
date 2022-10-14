40e27c9294d9cb17a1f7d204d14f6f0af107ed47
tracer-pa-public <tracer@njuics.org>
2022-10-14 20:58:24 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:58:24 up 86 days, 12:25,  2 users,  load average: 2.38, 2.44, 2.39 > unitime 1665752304269 > 4ad3d1baafa1dcd65e54934b2c9739e81807c40d

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -3,7 +3,7 @@
 Put the implementations of `call' instructions here.
 */
 make_instr_func(call_near)
-{
+{/*
 //    printf("callcallcall");
     OPERAND rel;
     rel.type = OPR_IMM;
@@ -23,5 +23,38 @@ make_instr_func(call_near)
     
     int offset = sign_ext(rel.val, data_size);
     cpu.eip += offset;
-    return 1 + data_size / 8;//???这个返回值什么用？
+    return 1 + data_size / 8;//???这个返回值什么用？*/
+
+    //与jmp类似，跳转前先将返回地址push进栈
+    cpu.gpr[4].val -= 4;
+
+    //将栈顶地址准备好
+    OPERAND stk_top;
+    stk_top.type = OPR_MEM;
+    stk_top.addr = cpu.gpr[4]._32;
+    stk_top.data_size = 32;
+    stk_top.sreg = SREG_CS;
+
+    //将eip值（返回地址）写进栈顶地址（入栈）
+    stk_top.val = cpu.eip + 1 + data_size / 8;
+    operand_write(&stk_top);
+
+    
+    OPERAND rel;
+    rel.type = OPR_IMM;
+    rel.sreg = SREG_CS;
+    rel.data_size = data_size;
+    rel.addr = cpu.eip + 1;
+    //the value of rel
+    operand_read(&rel);
+    
+    int offset = sign_ext(rel.val, data_size);
+
+    cpu.eip += offset;
+
+    //printf("the eip =  %x\n", cpu.eip);
+
+    return 1 + data_size / 8;
+    
+}
 }
