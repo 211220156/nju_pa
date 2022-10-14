d3911b31c04fec30cd765aa73ab202315d59b149
tracer-pa-public <tracer@njuics.org>
2022-10-14 11:25:42 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:25:42 up 86 days,  2:52,  2 users,  load average: 1.97, 2.01, 2.08 > unitime 1665717941953 > f2e06cc95df20dd6b2b9e831230a071a909db132

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -7,6 +7,7 @@ static void instr_execute_1op()
     operand_read(&opr_src);
     OPERAND top;
     top.data_size = data_size;
+    top.type = OPR_MEM;
     top.addr = cpu.gpr[4].val;
     operand_read(&top);
     cpu.gpr[4].val += (top.data_size / 8); 
