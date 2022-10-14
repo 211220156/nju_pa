4d7d8089d7ec26dd376bca2ef565f4409f186293
tracer-pa-public <tracer@njuics.org>
2022-10-14 11:05:21 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:05:21 up 86 days,  2:31,  2 users,  load average: 2.17, 2.25, 2.21 > unitime 1665716720780 > f6b534841db91798f33b1c148594dcf88ca6ea8c

--- nemu/include/cpu/instr/adc.h
+++ nemu/include/cpu/instr/adc.h
@@ -3,7 +3,7 @@
 /*
 Put the declarations of `adc' instructions here.
 */
-
+make_instr_func(adc_r2rm_v);
 
 
 #endif
--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -2,9 +2,11 @@
 /*
 Put the implementations of `adc' instructions here.
 */
-/*static void instr_execute_2op() 
+static void instr_execute_2op() 
 {
 	operand_read(&opr_src);
-	opr_dest.val = opr_dest.val + opr_src.val + cpu.eflags.CF;
+	operand_read(&opr_dest);
+	opr_dest.val = alu_add(opr_src.val + cpu.eflags.CF, opr_dest.val, data_size);
 	operand_write(&opr_dest);
-}*/
+}
+make_instr_impl_2op(adc, r, rm, v)
