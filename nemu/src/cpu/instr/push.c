393f2cb1fbf002d563925a27de429a4719474df9
tracer-pa-public <tracer@njuics.org>
2022-10-14 17:37:07 +0800

    > clean > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:37:07 up 86 days,  9:03,  2 users,  load average: 2.27, 2.13, 2.19 > unitime > f5083565226339872bc2a720d5a0e7905b57e503

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -6,6 +6,9 @@ static void instr_execute_1op()
 {
     operand_read(&opr_src);
     cpu.gpr[4].val -= (opr_src.data_size / 8);
+    if (opr_src.type == OPR_IMM) {
+        opr_src.val = sign_ext(opr_src.val);
+    }
     OPERAND opr = opr_src;
     opr.type = OPR_MEM;
     opr.addr = cpu.gpr[4].val;
