e7ad9ea0c6c5c2e8fce16210e3b14f9482d9cb4e
tracer-pa-public <tracer@njuics.org>
2022-10-15 11:46:00 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:46:00 up 87 days,  3:12,  2 users,  load average: 2.13, 2.16, 2.21 > unitime 1665805560218 > 7f5a0a94b21c391ca3bf1ea98fcd4bd7d409b84b

--- nemu/include/cpu/instr/sbb.h
+++ nemu/include/cpu/instr/sbb.h
@@ -3,5 +3,5 @@
 /*
 Put the declarations of `sbb' instructions here.
 */
-
+make_instr_func(sbb, rm, r, v);
 #endif
--- nemu/src/cpu/instr/sbb.c
+++ nemu/src/cpu/instr/sbb.c
@@ -2,3 +2,11 @@
 /*
 Put the implementations of `sbb' instructions here.
 */
+static void instr_execute_2op()
+{
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    opr_dest.val = alu_sbb(opr_src.val, opr_dest.val, data_size);
+    operand_write(&opr_dest);
+}
+make_instr_impl_2op(sbb, rm, r, v)
