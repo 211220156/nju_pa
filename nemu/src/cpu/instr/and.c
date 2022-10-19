11d6b68f63c9e88585826fb190ba97476d1fee40
tracer-pa-public <tracer@njuics.org>
2022-10-19 16:03:53 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:03:53 up 91 days,  7:30,  2 users,  load average: 1.71, 1.97, 2.12 > unitime 1666166633827 > e9e3e29f992c3b16a95e35d05ec4e259cad4ba75

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -6,7 +6,7 @@ static void instr_execute_2op()
 {
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    if (opr_src.data_size == 8)
+    if (opr_src.data_size == 8 && opr_src.data_size < opr_dest.data_size)
         opr_src.val = sign_ext(opr_src.val, opr_src.data_size);
     opr_dest.val = alu_and(opr_src.val, opr_dest.val, data_size);
     operand_write(&opr_dest);
