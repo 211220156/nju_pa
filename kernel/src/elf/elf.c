fb0be573382ed1012cfb99c0eb22293ad1c2bd96
tracer-pa-public <tracer@njuics.org>
2022-10-19 22:08:57 +0800

    > clean > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:08:57 up 91 days, 13:35,  3 users,  load average: 2.04, 2.24, 2.28 > unitime > ed5ed5c642f00f6f23ad5fd282d38eb042195fa3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -42,11 +42,11 @@ uint32_t loader()
 
 /* TODO: copy the segment from the ELF file to its proper memory area */
             for (int i = 0; i < ph->p_filesz; i++){
-                memset(ph->p_vaddr + i, ph + ph->p_offset + i, 1);
+                memset((void *)ph->p_vaddr + i, ph + ph->p_offset + i, 1);
             }
 
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
-            memset(ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+            memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
