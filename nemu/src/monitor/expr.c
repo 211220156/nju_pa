2b674b6c4e41b2bc7ed6813a946feaac076ed9c9
tracer-pa-public <tracer@njuics.org>
2022-10-28 19:48:28 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:48:28 up 100 days, 11:15,  2 users,  load average: 2.77, 2.49, 2.37 > unitime 1666957708544 > 70ffb53683b3b71fd64bc1fea11f68ebc6fbb69f

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -17,12 +17,17 @@ enum
 	NUM,
 	REG,
 	SYMB,
-
+    HEX,
+    DEREF,//解引用
+    NEG,//取负数
 	/* TODO: Add more token types */
+	LEFTP,
+	RIGHTP,
     DIV,
     MUL,
     ADD,
     SUB
+    
 };
 
 static struct rule
@@ -37,11 +42,12 @@ static struct rule
 
 	{" +", NOTYPE}, // white space
 	{"[0-9]{1,10}", NUM},
+	{"0[xX][0-9a-fA-F]{1, 8}", HEX},
     {"-", '-'},	
 	{"\\*", '*'},
-	{"\\(", '('},
-	{"\\)", ')'},
-	{"\\+", '+'}
+	{"\\(", LEFTP},
+	{"\\)", RIGHTP},
+	{"\\+", ADD}
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]))
@@ -104,6 +110,18 @@ static bool make_token(char *e)
 
 				switch (rules[i].token_type)
 				{
+				case HEX:
+				case NUM:
+				{
+				    tokens[nr_token].type = rules[i].token_type;
+				    if (substr_len > 32){
+				        memcpy((void *)tokens[nr_token].str, (void *)substr_start, 32);
+				    } else {
+				        memcpy((void *)tokens[nr_token].str, (void *)substr_start, substr_len);
+				    }
+					nr_token++;
+					break;
+				}
 				default:
 					tokens[nr_token].type = rules[i].token_type;
 					nr_token++;
@@ -131,9 +149,77 @@ uint32_t expr(char *e, bool *success)
 		return 0;
 	}
 
-	printf("\nPlease implement expr at expr.c\n");
-	fflush(stdout);
-	assert(0);
-
-	return 0;
+/*	printf("\nPlease implement expr at expr.c\n");
+    fflush(stdout);
+	assert(0);*/
+	for(i = 0; i < nr_token; i ++) {
+        if(tokens[i].type == '*' && (i == 0 || tokens[i - 1].type > 261)) {//若*前一位是运算符（>261） 
+            tokens[i].type = DEREF;
+        }
+        if (token[i].type == '-' && (i == 0 || tokens[i - 1].type > 261)){
+            tokens[i].type = NEG;
+        }
+    }
+    
+	return eval(0, nr_token - 1, success);
 }
+bool check_parentheses(int p, int q, bool* success)
+{
+    int stk[1000], top = -1, lastOutP = 0;
+    for (int i = p; i <= q; i++){
+        if (tokens[i].type == LEFTP)
+            stk[++top] = i;
+        else if (tokens[i].type == RIGHTP) {
+            if (top != -1)
+                lastOutP = stk[top--];
+            else {//出现多出来的右括号
+                *success = false;
+                return false; 
+            }
+        }
+    }
+    if (lastOutP == p && tokens[p].type == LEFTP && tokens[q].type == RIGHTP)
+        return true;
+    else 
+        return false;
+}
+uint32_t eval(int p, int q, bool* success)
+{
+    if(p > q) {
+        *success = false;
+        return 0;
+    }
+    else if(p == q) { 
+        /* Single token.
+         * For now this token should be a number. 
+         * Return the value of the number.
+         */ 
+        return atoi(tokens[p].str);
+    }
+    else if(check_parentheses(p, q, success) == true ) {
+        /* The expression is surrounded by a matched pair of parentheses. 
+         * If that is the case, just throw away the parentheses.
+         */
+        return eval(p + 1, q - 1); 
+    }
+    else {
+        //check返回false有两种可能：一种是表达式不合法，这时候success也会变成false，另一种是表达式合法，正常计算
+        if (*success == false){
+            return 0;
+        }
+        //下面是正常计算
+/*        op = the position of dominant operator in the token expression;
+        val1 = eval(p, op - 1);
+        val2 = eval(op + 1, q);
+        switch(op_type) {
+            case '+': return val1 + val2;
+            case '-': /* ... */
+ /*           case '*': /* ... */
+ /*           case '/': /* ... */
+ /*           default: assert(0);
+        
+        }*/
+        return 0;
+    }
+}
+
