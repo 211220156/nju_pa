ff6ee3a26cfe0cf38a81d1ec06b462dd04626de8
tracer-pa-public <tracer@njuics.org>
2022-10-29 10:48:16 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:48:16 up 101 days,  2:14,  2 users,  load average: 2.24, 2.21, 2.26 > unitime 1667011696325 > fc4bbbae7272e17cdb04f7225d7698849d96abff

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -230,10 +230,11 @@ uint32_t expr(char *e, bool *success)
 		return 0;
 	}
     printf("make_token ok!\n");
+    return 1;
 /*	printf("\nPlease implement expr at expr.c\n");
     fflush(stdout);
 	assert(0);*/
-	for(int i = 0; i < nr_token; i ++) {
+/*	for(int i = 0; i < nr_token; i ++) {
         if(tokens[i].type == '*' && (i == 0 || tokens[i - 1].type > 261)) {//若*前一位是运算符（>261） 
             tokens[i].type = DEREF;
         }
@@ -242,6 +243,6 @@ uint32_t expr(char *e, bool *success)
         }
     }
     
-	return eval(0, nr_token - 1, success);
+	return eval(0, nr_token - 1, success);*/
 }
 
