201e144ee0d4f3566e748c0f2d438460c6e562d0
tracer-pa-public <tracer@njuics.org>
2022-10-30 19:41:49 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:41:49 up 102 days, 11:08,  2 users,  load average: 3.03, 2.88, 2.64 > unitime 1667130109421 > eb417a7cf6ea079be835469749fda5e0c7fa5181

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -271,11 +271,11 @@ uint32_t eval(int p, int q, bool* success)
     }
     else {
         //check返回false有两种可能：一种是表达式不合法，这时候success也会变成false，另一种是表达式合法，正常计算
-//        printf("in eval!\n");
+        printf("in eval!\n");
         if (*success == false){
             return 0;
         }
-//        printf("in eval22222!\n");
+        printf("in eval22222!\n");
         //下面是正常计算
         int op = 0;
         bool inParentheses = false;
@@ -299,7 +299,7 @@ uint32_t eval(int p, int q, bool* success)
             case NEG:
             case SUB: return val1 - val2;
             case DEREF: {
-//                printf("in DEREF!\n");
+                printf("in DEREF!\n");
                 return vaddr_read(val2, SREG_CS, 4);
             }
             case MUL: return val1 * val2;
