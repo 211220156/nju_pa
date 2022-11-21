3d75e247163ab91221b0dfbf0f5f31442a07b47e
tracer-pa-public <tracer@njuics.org>
2022-11-21 16:16:45 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:16:45 up 124 days,  7:43,  2 users,  load average: 2.21, 2.23, 2.25 > unitime 1669018605470 > c19959946cbb63513b2e13701ce10813bc28043c

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -2,3 +2,18 @@
 /*
 Put the implementations of `lgdt' instructions here.
 */
+make_instr_func(lgdt){
+    int len = 1;
+    OPERAND opr;
+    //先读取16位的limit
+    opr.data_size = 16;
+    len += modrm_rm(eip + 1, &opr);
+    OPERAND_READ(&opr);
+    cpu.gdtr.limit = opr.val;
+    //继续读取32位的base，16位limit占两字节，因此opr的地址需要+2
+    opr.addr += 2;
+    opr.data_size = 32;
+    OPERAND_READ(&opr);
+    cpu.gdtr.base = opr.val;
+    return len;
+}
