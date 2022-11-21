f35337dc32e797abc5750d03fc7588044a634517
tracer-pa-public <tracer@njuics.org>
2022-11-21 20:15:59 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:15:59 up 124 days, 11:42,  2 users,  load average: 2.48, 2.47, 2.42 > unitime 1669032958852 > 729b57e2457e4ef9765c9d7a5fdef5796f33d2ad

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -18,13 +18,13 @@ void load_sreg(uint8_t sreg)
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-	uint32_t addr = (uint32_t)hw_mem + cpu.gdtr.base + cpu.segReg[sreg].index * 8;//sizeof(SegDesc);
+	uint32_t addr = (uint32_t)hw_mem + cpu.gdtr.base + cpu.segReg[sreg].index * sizeof(SegDesc);
 	SegDesc* curr = (void*)addr;
 	assert(curr->present == 1 && curr->granularity == 1);
 	uint32_t base = (curr->base_31_24 << 24) + (curr->base_23_16 << 16) + curr->base_15_0;
 	uint32_t limit = (curr->limit_19_16 << 16) + curr->limit_15_0;
 	assert(limit == 0xfffff && base == 0);
-	cpu.segReg[sreg].base = base;
-	cpu.segReg[sreg].limit = limit;
-	cpu.segReg[sreg].privilege_level = curr->privilege_level;
+	cpu.cache[sreg].base = base;
+	cpu.cache[sreg].limit = limit;
+	cpu.cache[sreg].privilege_level = curr->privilege_level;
 }
