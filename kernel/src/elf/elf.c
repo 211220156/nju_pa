a5334c466779a6ef871576dee685dc79383290d7
tracer-pa-public <tracer@njuics.org>
2022-12-07 22:46:22 +0800

    > compile nemu testcase kernel > id 211220156 > user pa211220156 > uname Linux f174fc791afd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:46:22 up 140 days, 14:12,  2 users,  load average: 2.16, 2.25, 2.34 > unitime 1670424382502 > 71cfd635a0ac533d1c1c31f05ccd63a8e33de395

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -40,12 +40,14 @@ uint32_t loader()
 			// remove this panic!!!
 			//panic("Please implement the loader");
             uint32_t paddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
-            ide_read((uint8_t *)paddr, ph->p_offset, ph->p_filesz);
+//            ide_read((uint8_t *)paddr, ph->p_offset, ph->p_filesz);
 /* TODO: copy the segment from the ELF file to its proper memory area */
 //            memcpy((void *)ph->p_vaddr, (void *)ph->p_offset, ph->p_filesz);
+            memcpy((void *)paddr, (void *)ph->p_offset, ph->p_filesz);
 
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
 //            memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+            memset((void *)paddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
