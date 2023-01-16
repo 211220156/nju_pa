75644743baab3fea03923668220ebd43e7dc037b
tracer-pa-public <tracer@njuics.org>
2023-01-16 19:40:23 +0800

    > clean > id 211220156 > user pa211220156 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 19:40:23 up  7:47,  1 user,  load average: 0.06, 0.05, 0.00 > unitime > 29e1cdd2635f5c1a8484ea98e37bebd88a87322e

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,18 +5,15 @@
 paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
-/*	printf("\nPlease implement page_translate()\n");
-	fflush(stdout);
-	assert(0);*/
 	
 	uint32_t dir = (laddr >> 22) & 0x3ff;
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
 	
 	PDE *pageDir = (PDE*)(hw_mem + (cpu.cr3.pdbr << 12) + (dir << 2));
-//	assert(pageDir->present == 1);
+	assert(pageDir->present == 1);
 	PTE *pageTable = (PTE*)(hw_mem + (pageDir->page_frame << 12) + (page << 2));
-//	assert(pageTable->present == 1);
+	assert(pageTable->present == 1);
 	
 	return (pageTable->page_frame << 12) + offset;
 	
